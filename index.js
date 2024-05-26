db03fd63c9388ed933012f65070b44f35869aa6d
Zeroget <nikitenko.fedor.2000@mail.ru>
2024-05-26 14:51:58 +0700

    improve pdf service

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const express = require('express');
 const cors = require('cors');
 const booleanParser = require('express-query-boolean');
 const numberParser = require('express-query-int');
-const {queryParser}  = require('express-query-parser');
+const bodyParser = require('body-parser');
+const { queryParser } = require('express-query-parser');
 
 const app = express()
 const port = 3000
 
 app.use(booleanParser());
 app.use(numberParser());
+app.use(bodyParser.text({ type: 'text/html' }));
 app.use(queryParser({
   parseNull: true,
   parseUndefined: true,
@@ -18,11 +20,52 @@ app.use(queryParser({
   parseNumber: true
 }));
 
+function getOptions(request) {
+  return { format: 'a4', landscape: false, printBackground: true, waitUntil: 'networkidle0', ...request.query, path: null };
+}
+
 app.get('/pdfs', cors(), async (request, response) => {
+  options = getOptions(request);
   const browser = await pdfService.launchBrowser();
-  const res = await pdfService.printPdf({ url: request.query.url, browser, options: {} });
+  try {
+    const result = await pdfService.getPdfFromUrl({ url: request.query.url, browser, options: options });
+    response.attachment(`document.pdf`).send(result);
+    return;
+  }
+  catch (err) {
+    response.sendStatus(400);
+    return;
+  }
+  finally {
+    await browser.close();
+  }
+});
+
+app.get('/healthcheck', cors(), async (request, response) => {
+  const browser = await pdfService.launchBrowser();
+  const result = await pdfService.getPdfFromUrl({ url: 'https://www.google.com/', browser, options: {} });
   await browser.close();
-  response.attachment(`name.pdf`).send(res);
+  response.sendStatus(200);
+});
+
+app.get('/', cors(), async (request, response) => {
+  response.sendStatus(200);
+});
+
+app.post('/pdfs', cors(), async (request, response) => {
+  options = getOptions(request);
+  const browser = await pdfService.launchBrowser();
+  try {
+    const result = await pdfService.getPdfFromHtml({ htmlContents: request.body, browser, options: options });
+    response.attachment(`document.pdf`).send(result);
+    return;
+  } catch (err) {
+    response.sendStatus(400);
+    return;
+   }
+  finally {
+    await browser.close();
+  }
 });
 
 app.options('/*', cors());
--- modules/pdf-service.js
+++ modules/pdf-service.js
@@ -2,16 +2,26 @@ const name = "pdfService";
 
 const puppeteer = require('puppeteer')
 
-exports.printPdf = async function ({ browser, url, options }) {
+exports.getPdfFromUrl = async function ({ browser, url, options }) {
   const page = await browser.newPage();
-  await page.goto(url);
+  //https://pptr.dev/api/puppeteer.puppeteerlifecycleevent
+  page.goto(url, {waitUntil: options.waitUntil});
+
   return page.pdf(options);
 }
 
+exports.getPdfFromHtml = async function ({ browser, htmlContents, options }) {
+  const page = await browser.newPage();
+  //https://pptr.dev/api/puppeteer.puppeteerlifecycleevent
+  await page.setContent(htmlContents, { waitUntil: options.waitUntil });
+  
+  return page.pdf(options);
+}
 
 exports.launchBrowser = function () {
   return puppeteer.launch({
     headless: 'new',
+    dumpio: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 }
